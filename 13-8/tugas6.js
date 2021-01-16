20d1e369bdaafb840800aff2c17630efc7442913
achiang <jecky_hindrawan123@yahoo.com>
2021-01-16 21:58:13 +0700

    still trying

--- 13-8/tugas6.js
+++ 13-8/tugas6.js
@@ -60,7 +60,7 @@ function howManyGifts(maxBudget, gifts) {
 }
 
 console.log(howManyGifts(30000, [15000, 12000, 5000, 3000, 10000])); // 4
-// console.log(howManyGifts(10000, [2000, 2000, 3000, 1000, 2000, 10000])); // 5
-// console.log(howManyGifts(4000, [7500, 1500, 2000, 3000])); // 2
-// console.log(howManyGifts(50000, [25000, 25000, 10000, 15000])); // 3
-// console.log(howManyGifts(0, [10000, 3000])); // 0
+console.log(howManyGifts(10000, [2000, 2000, 3000, 1000, 2000, 10000])); // 5
+console.log(howManyGifts(4000, [7500, 1500, 2000, 3000])); // 2
+console.log(howManyGifts(50000, [25000, 25000, 10000, 15000])); // 3
+console.log(howManyGifts(0, [10000, 3000])); // 0
